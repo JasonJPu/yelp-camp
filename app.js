3749c4c2c6062aea5e5294bd8f764a3dd6eee42e
JasonJPu <cub1@live.ca>
2017-07-05 06:08:47 -0400

    Add mongolab

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ const indexRoutes = require("./routes/index");
 
 const app = express();
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://admin:yelp-camp@ds149412.mlab.com:49412/yelp-camp");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "/public")));
